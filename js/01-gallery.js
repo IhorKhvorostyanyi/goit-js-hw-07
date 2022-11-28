b4a23ea823d26c6a77cbcf5755e63a426c716289
Ihor Khvorostyanyi <igor_gk@ukr.net>
2022-11-28 23:41:09 +0200

    Before last step

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+import * as basicLightbox from "./basiclightbox.min.js";
 console.log(galleryItems);
+
+// const basicLightbox = require("./basiclightbox");
+
+const makeSingleImageMarkup = ({ preview, original, description }, index) => {
+	return `<div class="gallery__item">
+    <a class="gallery__link" 
+        target="_blank"
+        rel="noopener noreferrer" 
+        href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+	`;
+};
+const makeImagesGallery = galleryItems.map(makeSingleImageMarkup).join("");
+// console.log(makeImagesGallery);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("afterbegin", makeImagesGallery);
+galleryEl.addEventListener("click", OnGalleryClick);
+
+function OnGalleryClick(event) {
+	console.log(event.target.dataset.source);
+	basicLightbox
+		.create(
+			`
+		<img width="1280" src=${event.target.dataset.source} >
+	`
+		)
+		.show();
+}
